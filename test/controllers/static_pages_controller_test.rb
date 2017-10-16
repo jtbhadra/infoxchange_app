b8b6211085856de0d2acec2ff84b3bd9d4fb053a
Jessi Bhadra <jessibhadra@gmail.com>
2017-10-16 01:28:43 -0700

    Add other pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
+  test "should get archive" do
+    get static_pages_archive_url
+    assert_response :success
+  end
+
+end
