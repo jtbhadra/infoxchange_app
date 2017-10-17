cdeb37c5ed3a763dc23a0ff6fc72954260bed722
Jessi Bhadra <jessibhadra@gmail.com>
2017-10-16 19:04:56 -0700

    Changle routes.rb to custom urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/archive'
-
-  get 'static_pages/about'
-
+  get '/archive', to: 'static_pages#archive'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
   root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
